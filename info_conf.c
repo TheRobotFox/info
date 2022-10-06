8c1e39947c33f9d68c2f25705f58c944c81b42a4
Tomke <pfoch.tomke@gmail.com>
2022-10-06 19:19:47 +0200

    deleted:    --force 	new file:   info_conf.c 	new file:   info_conf.h

---
+++ info_conf.c
@@ -0,0 +1,56 @@
+#include "info_internal.h"
+#include "format.h"
+//
+#define INFO_GENERATE_ANSI(ENUM) [ENUM] = (struct info_ANSI){1},
+#define INFO_GENERATE_STRING(STRING) [STRING] = INFO_STR(#STRING),
+#define INFO_GENERATE_FORMAT(STRING) [STRING] = {NULL,NULL},
+//
+//
+//
+#define FORMAT_DEFAULT INFO_STR("%([%t][%i] %d%{%f}[200,200,120]a: %)%<content>")
+#define NEWLINE_DEFAULT INFO_STR("\n%{%g}w")
+//
+#define FORMAT_STRUCTURED INFO_STR("%s%c")
+#define NEWLINE_STRUCTURED INFO_STR("\n%d")
+//
+//
+// set default colors for types
+const ANSI info_internal_ANSI[] = {
+	// set defaults
+	INFO_FOREACH_FUNC(INFO_GENERATE_ANSI)
+	[ZERO] = INFO_ANSI_normal_color(255,255,255),
+//
+	[INFO] = INFO_ANSI_normal_color(0, 200, 255),
+	[SEG] = INFO_ANSI_normal_color(200, 255, 0),
+	[ERROR] = INFO_ANSI_normal_color(200, 70, 70),
+	[FATAL] = INFO_ANSI_normal_color(255, 0, 0),
+	[SUCCESS] = INFO_ANSI_normal_color(0, 255, 0),
+};
+//
+// set type names
+const info_char *info_internal_tags[] = {
+	INFO_FOREACH_FUNC(INFO_GENERATE_STRING)
+	[FATAL] = INFO_STR("FATAL ERROR")
+};
+//
+// default formats
+info_formats info_internal_formats = {
+	INFO_FOREACH_FUNC(INFO_GENERATE_FORMAT)
+//
+	[FATAL] = {INFO_STR("%([%t][%i] At %{%F}[255][0][0]a:%L in %{%f}[255][0][0]a: %)%c"),
+             INFO_STR("\n%{%g}w")},
+
+	[ZERO]  = {INFO_STR("%(%d%)%c"),
+						 INFO_STR("\n%{%g}w")}
+};
+//
+// Message prefix ANSI
+ANSI ansi_prefix = {1};
+//
+const info_char *indent = INFO_STR("    ");
+//
+// example formats
+const struct info_format format_default = { FORMAT_DEFAULT, NEWLINE_DEFAULT};
+const struct info_format format_structured = { FORMAT_STRUCTURED, NEWLINE_STRUCTURED};
+//
+struct info_format format_current = format_default;
---
+++ info_conf.h
@@ -0,0 +1,31 @@
+#pragma once
+// configure info
+//
+// use wide characters
+#define INFO_WIDE
+//
+//
+// define your types here
+#define INFO_FOREACH_FUNC(FUNC) \
+        FUNC(INFO)     		    	\
+        FUNC(SUCCESS)	        	\
+        FUNC(ERROR)      	    	\
+        FUNC(SEG)     	      	\
+				FUNC(FATAL) 						\
+//
+enum INFO_TYPE
+{
+	ZERO,
+	INFO_FOREACH_FUNC(INFO_GENERATE_ENUM)
+//
+
+//
+	INFO_COUNT
+};
+//
+//
+//
+//
+//
+//
+//
